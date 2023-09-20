abf2cace644feffc3c3ffe1ef32f4508abe503c9
Syihabudin Rahmat Ramadhan <99803288+syihabudin081@users.noreply.github.com>
2023-09-20 23:32:45 +0700

    Time: 55 ms (82.69%), Space: 42.7 MB (42.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,19 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let arr = []
-for(let i=0;i<nums.length;i++){
-    for(let j=i+1;j<nums.length;j++){
-        let result = nums[i] + nums[j]
-        if(result == target ){
-            arr.push(i)
-            arr.push(j)
-            return arr
-        }
+function twoSum(nums, target) {
+  const numMap = {}; // Membuat objek untuk menyimpan angka yang telah dilihat
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (complement in numMap) {
+      return [numMap[complement], i];
     }
+    numMap[nums[i]] = i; // Menyimpan angka dan indeksnya dalam objek
+  }
+  return null; // Jika tidak ada solusi
 }
-
-
-
-};
