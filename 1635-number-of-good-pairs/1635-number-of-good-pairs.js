69b980a01494a4d4548d8fa1c24230b802aa343a
Syihabudin Rahmat Ramadhan <99803288+syihabudin081@users.noreply.github.com>
2023-09-21 00:28:25 +0700

    Time: 54 ms (44.99%), Space: 42 MB (43.49%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.js
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.js
@@ -3,17 +3,32 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-     const numFrequency = {}; 
-  let goodPairs = 0; 
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    if (numFrequency[num]) {
-      goodPairs += numFrequency[num];
-      numFrequency[num]++;
-    } else {
-      numFrequency[num] = 1;
-    }
-  }
+//      const numFrequency = {}; 
+//   let goodPairs = 0; 
+//   for (let i = 0; i < nums.length; i++) {
+//     const num = nums[i];
+//     if (numFrequency[num]) {
+//       goodPairs += numFrequency[num];
+//       numFrequency[num]++;
+//     } else {
+//       numFrequency[num] = 1;
+//     }
+//   }
   
-  return goodPairs;
-};
+//   return goodPairs;
+// };
+
+const numfreq = {}
+let count = 0
+
+for(let i=0;i<nums.length;i++){
+    let num = nums[i]
+    if(numfreq[num]){
+        count = count + numfreq[num]
+        numfreq[num]++
+    }else{
+        numfreq[num] = 1
+    }
+}
+return count
+}
