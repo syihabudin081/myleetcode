df8ec0e73634d71eb3dc0b6ec576196766dab667
Syihabudin Rahmat Ramadhan <99803288+syihabudin081@users.noreply.github.com>
2023-09-21 01:05:01 +0700

    Time: 67 ms (54.74%), Space: 45.6 MB (46.44%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.js
+++ 2058-concatenation-of-array/2058-concatenation-of-array.js
@@ -6,7 +6,6 @@ var getConcatenation = function(nums) {
   let ans =[]
   let n = nums.length 
   console.log(n)
-  let anslen = n*2
   for(let i=0;i<n;i++){
         ans[i] = nums[i]
       ans[i+n] = nums[i]
