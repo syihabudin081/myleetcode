9b72002eda60d74c712695a8fc84b9a625043640
Syihabudin Rahmat Ramadhan <99803288+syihabudin081@users.noreply.github.com>
2023-09-21 01:29:12 +0700

    Time: 73 ms (5.32%), Space: 41.7 MB (96.81%) - LeetHub

--- 1975-minimum-distance-to-the-target-element/1975-minimum-distance-to-the-target-element.js
+++ 1975-minimum-distance-to-the-target-element/1975-minimum-distance-to-the-target-element.js
@@ -5,13 +5,14 @@
  * @return {number}
  */
 var getMinDistance = function(nums, target, start) {
-   let mindistance = Infinity
+    let mindistance = Infinity;
 
-    for (let i=0;i<nums.length;i++){
-        if(nums[i] == target){
-            let dis= Math.abs(i-start)
-            mindistance = Math.min(dis,mindistance)
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            const currentDistance = Math.abs(i - start);
+            mindistance = Math.min(currentDistance, mindistance);
         }
     }
-    return mindistance
+
+    return mindistance;
 };
